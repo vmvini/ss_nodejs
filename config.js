46d3063380a852931199d79f6332335cd0ee541a
Marcus Vinícius <vmvini@hotmail.com>
2016-03-13 22:44:29 -0300

    right database authentication config

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-	"database":"http://vmvini:123456@localhost:7474",
+	"database":"http://neo4j:iniv10x@localhost:7474",
 	"port": process.env.PORT || 3010,
 	"secretKey":"_312983y921uh_3hJHDHE9Hi_e29ehHIU(HE192HJHHiu329)ePOPOPOeewe12//???..../wqw1"
 }
