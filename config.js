70fd91b36dd7779ab9fa65c3bd6cf3d3011bc277
Marcus Vinicius <vmvini@hotmail.com>
2016-05-21 23:57:26 -0300

    added heroku dabase url

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 module.exports = {
-	"database":"http://neo4j:iniv10x@localhost:7474",
+	//"database":"http://neo4j:iniv10x@localhost:7474",
+	"database":process.env.GRAPHENEDB_URL,
 	"port": process.env.PORT || 8081,
 	"secretKey":"_312983y921uh_3hJHDHE9Hi_e29ehHIU(HE192HJHHiu329)ePOPOPOeewe12//???..../wqw1"
 }
