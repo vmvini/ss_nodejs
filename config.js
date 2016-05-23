252a4ac7026529f927392ef0ba9c03e07d31e3fc
Marcus Vinicius <vmvini@hotmail.com>
2016-05-22 21:00:46 -0300

    right database connection url

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-	"database":"http://neo4j:iniv10x@localhost:7474",
+	"database":"http://app51125953-H6mjI9:zpRJIIcWsk63eLMEzgmJ@app51125953h6mji9.sb05.stations.graphenedb.com:24789",
 	"port": process.env.PORT || 3010,
 	"secretKey":"_312983y921uh_3hJHDHE9Hi_e29ehHIU(HE192HJHHiu329)ePOPOPOeewe12//???..../wqw1"
 }
